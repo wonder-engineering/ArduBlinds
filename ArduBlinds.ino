b368841b2662971688756af4dabdb6e1140cd97e
bmleedy <bmleedy@gmail.com>
2019-01-06 18:08:42 -0800

    Linting success for my sketch

--- ArduBlinds.ino
+++ ArduBlinds.ino
@@ -31,7 +31,7 @@ Stepper stepper(STEPPER_STEPS_PER_REVOLUTION, STEPPER_PIN_1,
 
 void setup() {
   stepper.setSpeed(12);
-  
+
   // initialize the serial port:
   Serial.begin(57600);
 
@@ -40,32 +40,37 @@ void setup() {
   digitalWrite(LED_PIN, 0);
 
   // setup button pins with internal pullup
-  pinMode(UP_BUTTON_PIN,  INPUT_PULLUP);
-  pinMode(DOWN_BUTTON_PIN,INPUT_PULLUP);
-  pinMode(LIMIT_PIN,      INPUT_PULLUP);
+  pinMode(UP_BUTTON_PIN,   INPUT_PULLUP);
+  pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LIMIT_PIN,       INPUT_PULLUP);
 
   // setup the analog input pin (external pullup)
-  pinMode(SENSOR_PIN,INPUT);
+  pinMode(SENSOR_PIN, INPUT);
 }
 
-#define MOTOR_POSITION_OPEN  0                   // at powerup, assumes we're here
-#define ONE_HUNDREDTH_MOTOR_POSITION_CLOSED 300  // moves to here if too light
+#define MOTOR_POSITION_OPEN  0                   // at powerup,
+                                                 // assumes we're here
+#define ONE_HUNDREDTH_MOTOR_POSITION_CLOSED 300  // moves to here
+                                                 // if too light
 
 int upButtonState   = 1;     // 1 = open, 0 = pressed
 int downButtonState = 1;     // 1 = open, 0 = pressed
 int light_sensor_value = 0;  // Dark = 1023; pointing at bulb = 41
-#define LIGHT_SENSOR_DARK_THRESHOLD    800  // less than this will trigger motion
-#define LIGHT_SENSOR_LIGHT_THRESHOLD   400  // more than this will trigger motion
-#define LIGHT_SENSOR_WAIT_PERIOD       200  // must breach threshold for this many consec. cycles
+#define LIGHT_SENSOR_DARK_THRESHOLD    800  // less than this will
+                                            // trigger motion
+#define LIGHT_SENSOR_LIGHT_THRESHOLD   400  // more than this will
+                                            // trigger motion
+#define LIGHT_SENSOR_WAIT_PERIOD       200  // must breach threshold
+                                            // for this many consec. cycles
 #define LIGHT_SENSOR_DELAY_PER_CYCLE   10   // duration of cycles above
-int bright_counter = 0;      
+int bright_counter = 0;
 int dark_counter = 0;
-int motor_position = 0;            //current motor position
-int desired_motor_position = 0;    //current motor position
+int motor_position = 0;            // current motor position
+int desired_motor_position = 0;    // current motor position
 
 enum ShadeState {bright, neutral, dark};
 ShadeState sensor_state   = dark;   // the desired position
-ShadeState shade_position = dark; // the actual position
+ShadeState shade_position = dark;   // the actual position
 bool led_state = false;
 
 void loop() {
@@ -76,7 +81,7 @@ void loop() {
 
 
   // Clear sensor counters and state when you press any button
-  if(upButtonState == 0 && downButtonState ==0){
+  if (upButtonState == 0 && downButtonState ==0) {
     sensor_state   = neutral;
     bright_counter = 0;
     dark_counter   = 0;
@@ -84,38 +89,41 @@ void loop() {
 
   // Act on Button and sensor data
   if (upButtonState == 0 && downButtonState ==0) {
-    Serial.println(F("Both buttons pressed. Setting shade position to 'dark' position!"));
+    Serial.println(F("Both buttons pressed. Setting shade "
+                     "position to 'dark' position!"));
     shade_position = dark;
   } else if (upButtonState == 0) {
-    Serial.println("Stepping from dark to light position. ('up', 'blinds closed')");
+    Serial.println("Stepping from dark to light position. ('up', "
+                   "'blinds closed')");
     while (digitalRead(UP_BUTTON_PIN) == 0)
       stepper.step(1);
   } else if (downButtonState == 0) {
-    Serial.println(F("Stepping from light toward dark position. ('down', 'blinds open')"));
-    Serial.println(F(" To calibrate the string position to the 'blinds open' position,"));
+    Serial.println(F("Stepping from light toward dark position."
+                     " ('down', 'blinds open')"));
+    Serial.println(F(" To calibrate the string position "
+                     "to the 'blinds open' position,"));
     Serial.println(F("   press both buttons at once."));
     while (digitalRead(DOWN_BUTTON_PIN) == 0)
       stepper.step(-1);
-  }
-  else if (light_sensor_value < LIGHT_SENSOR_LIGHT_THRESHOLD) { //too bright!
+  } else if (light_sensor_value < LIGHT_SENSOR_LIGHT_THRESHOLD) {
     bright_counter++;
     dark_counter = 0;
-    if(bright_counter > LIGHT_SENSOR_WAIT_PERIOD &&
+    if (bright_counter > LIGHT_SENSOR_WAIT_PERIOD &&
        sensor_state != bright) {
       bright_counter = 0;
       sensor_state = bright;
-      Serial.print("LIGHT condition detected! Last sensor value: "); 
+      Serial.print("LIGHT condition detected! Last sensor value: ");
       Serial.println(light_sensor_value);
     }
     // move the stepper to the closed position, if it's not already there
   } else if (light_sensor_value > LIGHT_SENSOR_DARK_THRESHOLD) {
     dark_counter++;
     bright_counter = 0;
-    if(dark_counter > LIGHT_SENSOR_WAIT_PERIOD  &&
+    if (dark_counter > LIGHT_SENSOR_WAIT_PERIOD  &&
               sensor_state != dark) {
       dark_counter = 0;
       sensor_state = dark;
-      Serial.print(F("DARK condition detected! Last sensor value: ")); 
+      Serial.print(F("DARK condition detected! Last sensor value: "));
       Serial.println(light_sensor_value);
     }
   } else {
@@ -129,51 +137,47 @@ void loop() {
   if (sensor_state != neutral && shade_position != sensor_state) {
     digitalWrite(LED_PIN, 1);  // indicate motion with built in LED
     Serial.println(F("Moving motor!"));
-    
-    if(sensor_state == dark) {
+
+    if (sensor_state == dark) {
       int iterator = 1;
-      while(digitalRead(LIMIT_PIN) == 0) {
-        stepper.step( (-1)*ONE_HUNDREDTH_MOTOR_POSITION_CLOSED );
-        iterator ++;
+      while (digitalRead(LIMIT_PIN) == 0) {
+        stepper.step((-1)*ONE_HUNDREDTH_MOTOR_POSITION_CLOSED);
+        iterator++;
         Serial.print(".");
         if (iterator % 10 == 0)
           Serial.println(iterator);
       }
     } else {
       // execute 100 slices of the steps I need to go
-      for(int i=0; i<100; i++) {
-  
+      for (int i = 0; i < 100; i++) {
         // Move in correct direction
-        if (sensor_state == bright)
-          stepper.step( ONE_HUNDREDTH_MOTOR_POSITION_CLOSED );
-        else
-          
-  
+        stepper.step(ONE_HUNDREDTH_MOTOR_POSITION_CLOSED);
+
         // blink the LED
         led_state = !led_state;
-        if(led_state)
+        if (led_state)
           digitalWrite(LED_PIN, 0);
         else
           digitalWrite(LED_PIN, 1);
-  
+
         // check the state of the buttons and break out if I'm interrupted
         if (digitalRead(UP_BUTTON_PIN) == 0 ||
             digitalRead(DOWN_BUTTON_PIN) ==0) {
-          Serial.print(F("Button hit!  Exiting motion at position iteration: ")); Serial.println(i);
+          Serial.print(F("Button hit!  Exiting motion "
+                         "at position iteration: "));
+          Serial.println(i);
           break;
         } else {
           if (i > 0 && i%10 == 0) {
-            Serial.print(i); 
-            Serial.println(F("\%"));
-          }
-          else if (i > 0){
+            Serial.print(i);
+            Serial.println(F("%"));
+          } else if (i > 0) {
             Serial.print(".");
           }
         }
-      } //for()
+      }  // for()
     }
 
-    
     Serial.println("Motion DONE!");
     shade_position = sensor_state;
 
@@ -186,6 +190,5 @@ void loop() {
   digitalWrite(LED_PIN, 0);
 
   delay(LIGHT_SENSOR_DELAY_PER_CYCLE);
-      
 }  // loop()
 
